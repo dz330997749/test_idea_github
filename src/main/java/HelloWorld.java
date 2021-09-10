c2a2158100d7d2f032a205411fdfdcd1dcc2c8b4
luojie <330997749@qq.com>
2021-09-10 21:53:07 +0800

    Initial commit

---
+++ src/main/java/HelloWorld.java
@@ -0,0 +1,14 @@
+/**
+ * @Project: test_idea_github
+ * @Site: http://www.lemonban.com
+ * @Forum: http://testingpai.com
+ * @Copyright: ©2021 版权所有 湖南省零檬信息技术有限公司
+ * @Author: luojie
+ * @Create: 2021-09-10 21:51
+ * @Desc：
+ **/
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World...");
+    }
+}
